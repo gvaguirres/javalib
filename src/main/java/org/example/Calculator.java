f5f689c1e82dd53c57f24ec45626d734d47274a7
Gabriela Aguirre <gvaguirres@gmail.com>
2026-01-22 21:01:24 +0100

    Adds Tests

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Calculator {
+//control + enter -> generate test
+//tres puntos arriba a la derecha -> run with Coverage
+//Coverage nos va a mostrar cuánto del código está cubierto por los tests
 
     public static int add(int a, int b){
         return a + b;
