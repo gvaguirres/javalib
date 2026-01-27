aee3da0145a63f3c4d4366a78dc4d070a6b5e552
Gabriela Aguirre <gvaguirres@gmail.com>
2026-01-27 13:34:58 +0100

    Update bowling class and add a onePinEachRollGivesScore20 test

--- src/main/java/org/example/bowling/Bowling.java
+++ src/main/java/org/example/bowling/Bowling.java
@@ -1,11 +1,13 @@
 package org.example.bowling;
 
 public class Bowling {
+    private int score = 0;
 
     public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- src/test/java/org/example/bowling/BowlingTest.java
+++ src/test/java/org/example/bowling/BowlingTest.java
@@ -7,14 +7,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class BowlingTest {
 
     @Test
-    void gutterGameGivesZeroScore() {
+    void gutterGameGivesScoreZero() {
         Bowling bowlingGame = new Bowling();
 
         for (int i = 0; i < 20; i++)
             bowlingGame.roll(0);
 
         assertThat(bowlingGame.score()).isEqualTo(0);
-
-
     }
+
+    @Test
+    void onePinEachRollGivesScore20(){
+        Bowling bowlingGame = new Bowling();
+
+        for (int i = 0; i < 20; i++)
+            bowlingGame.roll(1);
+
+        assertThat(bowlingGame.score()).isEqualTo(20);
+    }
+
+
 }
